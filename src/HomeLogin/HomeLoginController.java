bf2ad31376775429c6678ed7f4c17ac1b03e3208
thanhlong2001 <phucvatu456@gmail.com>
2024-06-22 06:42:27 -0800

    commit

--- src/HomeLogin/HomeLoginController.java
+++ src/HomeLogin/HomeLoginController.java
@@ -11,11 +11,6 @@ import javafx.fxml.FXML;
 
 public class HomeLoginController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
-
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
 
     @FXML
     void handleButtonAdmin(ActionEvent event) {
@@ -37,7 +32,6 @@ public class HomeLoginController {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
 
     }
