07caa9229a2229cb7eee97ff564e8c39843491fe
thanhlong2001 <phucvatu456@gmail.com>
2024-06-21 20:42:53 -0800

    commit
    comit

---
+++ src/Database/ConnectDB.java
@@ -0,0 +1,28 @@
+
+package Database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConnectDB {
+       Connection con=null;
+       
+ public Connection GetConnectDB(){
+        try{
+       
+            String url="jdbc:sqlserver://localhost:1433;databaseName=ProjectHoteljava1";
+            String user="sa";
+            String password="1";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con= DriverManager.getConnection(url,user,password);
+            System.out.println("Connect Sucessful");
+    }catch(Exception e){
+            System.out.println("Cant connect: " + e.getMessage());
+    }
+        return con;
+}
+     
+}
+
--- src/projecthotelmanagement2/FXMLDocumentController.java
+++ src/projecthotelmanagement2/FXMLDocumentController.java
@@ -9,7 +9,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -17,10 +23,8 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
     
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -30,5 +34,14 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonSignIn(ActionEvent event) {
+    
+    }
+
+    @FXML
+    private void handleButtonSIGNUP(ActionEvent event) {
+    }
     
 }
